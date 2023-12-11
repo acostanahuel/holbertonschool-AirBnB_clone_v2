66171e8053e3f5d0b6651eff0d9b5ba6c22a4c76
acostanahuel <acostanahuel@gmail.com>
2023-12-11 18:49:09 -0300

    update

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,15 +115,44 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+
+        argsTokens = args.partition(" ")
+
+        if not argsTokens[0]:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif argsTokens[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        tokenizedDict = {}
+        attributes = argsTokens[2:][0].split(" ")
+
+        for attr in attributes:
+
+            tokenKV = attr.split('=')
+
+            if (len(tokenKV) == 2):
+
+                if (tokenKV[1].startswith('"')):
+
+                    resStr = tokenKV[1].replace('_', ' ').replace('"', '')
+                    tokenizedDict[tokenKV[0]] = resStr
+
+                elif ('.' in tokenKV[1]):
+                    tokenizedDict[tokenKV[0]] = float(tokenKV[1])
+                else:
+                    tokenizedDict[tokenKV[0]] = int(tokenKV[1])
+
+        from datetime import datetime
+
+        if len(tokenizedDict) > 0:
+            tokenizedDict['created_at'] = datetime.now().isoformat()
+            tokenizedDict['updated_at'] = datetime.now().isoformat()
+
+        new_instance = HBNBCommand.classes[argsTokens[0]](**tokenizedDict)
         print(new_instance.id)
+        storage.new(new_instance)
         storage.save()
 
     def help_create(self):
@@ -187,7 +216,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -200,17 +229,16 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
         print_list = []
-
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(HBNBCommand.classes[args]).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
@@ -272,7 +300,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +308,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +347,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
