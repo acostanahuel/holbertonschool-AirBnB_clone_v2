0ada9cd3b74d08ad12ba61ade0defae6ee5f239d
acostanahuel <acostanahuel17@gmail.com>
2023-12-13 19:58:00 -0300

    fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -19,11 +19,13 @@ def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
@@ -31,12 +33,11 @@ def python(text="is cool"):
         text = text.replace("_", " ")
         return "Python {}".format(text)
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
         """Displays 'n is a number' only if n is an integer."""
         return "{} is a number".format(n)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    
-    
+    app.run(host="0.0.0.0", port=5000)    
