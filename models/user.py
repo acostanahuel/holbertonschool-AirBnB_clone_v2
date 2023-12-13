f18893464a4359c8ca96061f7d29623cad99c100
acostanahuel <acostanahuel17@gmail.com>
2023-12-13 19:21:18 -0300

    change everything

--- models/user.py
+++ models/user.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+"""Defines the User class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-from os import getenv
 
-typeStorage = getenv("HBNB_TYPE_STORAGE")
+
 
 
 class User(BaseModel, Base):
 
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-
-    if typeStorage == 'db':
-        places = relationship('Place', backref="user", cascade="all, delete")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
