67ac86b4b34de23d589283e068e0faa3e571d3e4
acostanahuel <acostanahuel17@gmail.com>
2023-12-17 16:11:36 -0300

    7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,4 +79,7 @@ class DBStorage:
         Session = scoped_session(session_factory)
         self.__session = Session()
 
+    def close(self):
+        """Close the working SQLAlchemy session."""
+        self.__session.close()
         
